53ef49c06dc711289a0e9bd604009768cd0e64b1
Julian Ghionoiu <julian.ghionoiu@valtech.co.uk>
2015-10-16 21:01:03 +0100

    Released new version to test CodeShip

--- lib/tdl/version.rb
+++ lib/tdl/version.rb
@@ -1,5 +1,5 @@
 module TDL
-  PREVIOUS_VERSION = '0.1.1'
+  PREVIOUS_VERSION = '0.1.2'
   # the current MAJOR.MINOR version is dynamically computed from the version of the Spec
-  CURRENT_PATCH_VERSION = '2'
+  CURRENT_PATCH_VERSION = '3'
 end
