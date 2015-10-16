cbef1fd7f3cc5891ce16a8ce9e89b18a1dde57ac
Julian Ghionoiu <julian.ghionoiu@valtech.co.uk>
2015-10-16 21:19:37 +0100

    Next patch number

--- lib/tdl/version.rb
+++ lib/tdl/version.rb
@@ -1,5 +1,5 @@
 module TDL
-  PREVIOUS_VERSION = '0.1.2'
+  PREVIOUS_VERSION = '0.1.3'
   # the current MAJOR.MINOR version is dynamically computed from the version of the Spec
-  CURRENT_PATCH_VERSION = '3'
+  CURRENT_PATCH_VERSION = '4'
 end
