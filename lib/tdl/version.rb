7e864fb7f00dd063f46283919432411e7a9a8f33
Julian Ghionoiu <julian.ghionoiu@valtech.co.uk>
2016-01-15 23:12:53 +0000

    Deployed 0.5.0 release

--- lib/tdl/version.rb
+++ lib/tdl/version.rb
@@ -1,5 +1,5 @@
 module TDL
-  PREVIOUS_VERSION = '0.3.6'
+  PREVIOUS_VERSION = '0.5.0'
   # the current MAJOR.MINOR version is dynamically computed from the version of the Spec
-  CURRENT_PATCH_VERSION = '7'
+  CURRENT_PATCH_VERSION = '0'
 end
