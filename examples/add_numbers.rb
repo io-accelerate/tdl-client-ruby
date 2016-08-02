963f0ff708216fd6b1468ebfde4f5bbe3c62e824
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2016-08-02 20:47:21 +0100

    Update example

--- examples/add_numbers.rb
+++ examples/add_numbers.rb
@@ -6,25 +6,25 @@ Logging.logger.root.level = :info
 
 
 def run_client
-  client = TDL::Client.new('localhost', 61613, 'julian')
+  client = TDL::Client.new( hostname: 'localhost', port: 61613, unique_id: 'julian@example.com')
   include TDL::ClientActions
   rules = TDL::ProcessingRules.new
   rules.on('display_description').call(method(:display)).then(publish)
-  rules.on('display_required_methods').call(method(:display)).then(publish)
-  rules.on('increment').call(method(:increment)).then(publish_and_stop)
+  rules.on('sum').call(method(:sum)).then(publish)
+  rules.on('end_round').call(lambda { 'OK' }).then(publish_and_stop)
 
   # puts processing_rules.to_yaml
 
   client.go_live_with(rules)
 end
 
-def display(str)
-  puts str
+def display(round, description)
+  puts "#{round} = #{description}"
   'OK'
 end
 
-def increment(x)
-  x + 1
+def sum(x, y)
+  x + y
 end
 
 
