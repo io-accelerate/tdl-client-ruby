83ef86653493ead12c38267932bab1e6d2554001
Egor Raskhodchikov <raskhodchikov.egor@gmail.com>
2018-02-07 18:17:15 +0700

    Remove old client code

--- lib/tdl.rb
+++ lib/tdl.rb
@@ -1,4 +1,3 @@
-require 'tdl/queue/client'
 require 'tdl/queue/implementation_runner_config'
 require 'tdl/queue/queue_based_implementation_runner'
 require 'tdl/queue/queue_based_implementation_runner_builder'
